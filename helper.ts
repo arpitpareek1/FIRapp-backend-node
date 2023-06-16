598cf481d87438a402f336c26af61351c4a04ed6
arpitpareek1 <128892280+arpitpareek1@users.noreply.github.com>
2023-06-17 00:44:56 +0530

    Update helper.ts remove some consoles and imporove code
    okay bye

--- helper.ts
+++ helper.ts
@@ -6,9 +6,7 @@ export const login = (req: Request, res: Response) => {
   const { email, password } = data
   let sql = `SELECT * from user where email = "${email}"`
   pool.query(sql, (err: any, data: any) => {
-    console.log(data)
     if (data && data.length > 0) {
-      console.log(data[0].password, password)
       if (data[0].password === password) {
         let res1 = {
           "data": data,
@@ -25,7 +23,6 @@ export const login = (req: Request, res: Response) => {
         }
       }
     } else {
-      console.log(err)
       let res1 = {
         "data": 'email not found',
         'success': false
@@ -36,14 +33,11 @@ export const login = (req: Request, res: Response) => {
 
   })
 
-
 }
 
 export function getallfir(req: Request, res: Response) {
-  console.log("geting user data...")
   let sql = "SELECT * from incident"
   pool.query(sql, (err: any, data: any) => {
-    console.log(data)
     if (data) {
       let response = {
         "data": data,
@@ -60,14 +54,10 @@ export function getallfir(req: Request, res: Response) {
 }
 
 export const signup = (req: Request, res: Response) => {
-  console.log("signuping user data...")
   let data = req.body
   const { username, password, number, email, role } = data
-  console.log(username, password, number, email, role)
   let sql = `SELECT * from user where email = ?`
   pool.query(sql, [email], (err: any, data1: any) => {
-    console.log(err)
-    console.log('---', data)
     if (data1.length > 0) {
       let response = {
         "resullt": 'user already have account',
@@ -116,11 +106,11 @@ export const craeteFir = (req: Request, res: Response) => {
     witnessAddress,
     id
   } = data
-  console.log('---req.data', data)
+
   let sql = `INSERT INTO incident (date, userid, time, fullName, contactNumber, address, incidentDate, incidentTime, location, incidentType, incidentDescription, witnessName, witnessContactNumber, witnessAddress, status) VALUES ( '${date}',${id}, '${time}','${fullName}','${contactNumber}','${address}','${incidentDate}','${incidentTime}','${location}','${incidentType}','${incidentDescription}','${witnessName}','${witnessContactNumber}','${witnessAddress}', "pending")`
-  console.log(sql)
+
   pool.query(sql, (err: any, data: any) => {
-    console.log(err, data)
+
     if (data) {
       let response = {
         'success': true
@@ -136,13 +126,11 @@ export const craeteFir = (req: Request, res: Response) => {
   })
 }
 export const setStatusSolve = (req: Request, res: Response) =>{
-  console.log(req.body)
+
    let id =  req.body.id
    let status =  req.body.status
-   console.log(id, status, '----------------------------------------------------------------')
   let sql = `UPDATE incident SET status = '${status}' WHERE id = ${id}`
   pool.query(sql, (err: any, data: any) => {
-    console.log(data)
     if (data) {
       let response = {
         'success': true
@@ -159,10 +147,8 @@ export const setStatusSolve = (req: Request, res: Response) =>{
 export const myCompaines =(req: Request, res: Response)=>{
   const data = req.body 
   const {id } = data
-  console.log(data, id)
   let sql = `select * from incident where userid = "${id}"`
   pool.query(sql, (err: any, data: any) => {
-    console.log(data)
     if (data) {
       let response = {
         "data": data,
